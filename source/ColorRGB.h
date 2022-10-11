2694886fbbeb1141b337d99aa47b3abfb1cfe202
Jolan Plaum <jolan.plaum@student.howest.be>
2022-10-11 20:12:44 +0200

    Added Fresnel base reflectivity colors

--- source/ColorRGB.h
+++ source/ColorRGB.h
@@ -146,4 +146,23 @@ namespace dae
 		static ColorRGB Black{ 0,0,0 };
 		static ColorRGB Gray{ 0.5f,0.5f,0.5f };
 	}
+
+	namespace fresnel
+	{
+		static ColorRGB Plastic		{ 0.040f, 0.040f, 0.040f };
+
+		static ColorRGB Titanium	{ 0.542f, 0.497f, 0.449f };
+		static ColorRGB Chromium	{ 0.549f, 0.556f, 0.554f };
+		static ColorRGB Iron		{ 0.562f, 0.565f, 0.578f };
+		static ColorRGB Nickel		{ 0.660f, 0.609f, 0.526f };
+		static ColorRGB Platinum	{ 0.673f, 0.637f, 0.585f };
+		static ColorRGB Copper		{ 0.955f, 0.638f, 0.538f };
+		static ColorRGB Palladium	{ 0.733f, 0.697f, 0.652f };
+		static ColorRGB Mercury		{ 0.781f, 0.780f, 0.778f };
+		static ColorRGB Brass		{ 0.910f, 0.778f, 0.423f };
+		static ColorRGB Zinc		{ 0.664f, 0.824f, 0.850f };
+		static ColorRGB Gold		{ 1.000f, 0.782f, 0.344f };
+		static ColorRGB Aluminum	{ 0.913f, 0.922f, 0.924f };
+		static ColorRGB Silver		{ 0.972f, 0.960f, 0.915f };
+	}
 }
