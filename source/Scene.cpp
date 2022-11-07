3a0412b828501f1ea0fb6377fd9f8463c71b9708
Jolan Plaum <jolan.plaum@student.howest.be>
2022-11-07 16:59:00 +0100

    Added test scene W4

--- source/Scene.cpp
+++ source/Scene.cpp
@@ -258,5 +258,50 @@ namespace dae {
 		AddPointLight({ -2.5f,	5.f,	-5.f }, 70.f, ColorRGB{ 1.f, 0.8f, 0.45f }); //Front Light Left
 		AddPointLight({ 2.5f,	2.5f,	-5.f }, 50.f, ColorRGB{ 0.34f, 0.47f, 0.68f });
 	}
+
+	void Scene_W4_TestScene::Initialize()
+	{
+		m_Camera.origin = { 0.f, 1.f, -5.f };
+		m_Camera.fovAngle = 45.f;
+		m_Camera.fov = tanf(m_Camera.fovAngle / 2.f * TO_RADIANS);
+
+		//Materials
+		const unsigned char matLambert_GrayBlue = AddMaterial(new Material_Lambert({ 0.49f, 0.57f, 0.57f }, 1.f));
+		const unsigned char matLambert_White = AddMaterial(new Material_Lambert(colors::White, 1.f));
+
+		//Planes
+		AddPlane({ 0.f, 0.f, 10.f }, { 0.f, 0.f, -1.f }, matLambert_GrayBlue); //BACK
+		AddPlane({ 0.f, 0.f, 0.f }, { 0.f, 1.f, 0.f }, matLambert_GrayBlue); //BOTTOM
+		AddPlane({ 0.f, 10.f, 0.f }, { 0.f, -1.f, 0.f }, matLambert_GrayBlue); //TOP
+		AddPlane({ 5.f, 0.f, 0.f }, { -1.f, 0.f, 0.f }, matLambert_GrayBlue); //RIGHT
+		AddPlane({ -5.f, 0.f, 0.f }, { 1.f, 0.f, 0.f }, matLambert_GrayBlue); //LEFT
+
+		//Triangle Mesh
+		pMesh = AddTriangleMesh(TriangleCullMode::BackFaceCulling, matLambert_White);
+		//Utils::ParseOBJ("Resources/simple_cube.obj",
+		//Utils::ParseOBJ("Resources/simple_object.obj",
+		Utils::ParseOBJ("Resources/simple_quad.obj",
+			pMesh->positions,
+			pMesh->normals,
+			pMesh->indices);
+
+		pMesh->Scale({ 0.7f, 0.7f, 0.7f });
+		pMesh->Translate({ 0.f, 1.f, 0.f });
+
+		pMesh->UpdateTransforms();
+
+
+		//Lights
+		AddPointLight({ 0.f,	5.f,	5.f }, 50.f, ColorRGB{ 1.f, 0.61f, 0.45f }); //Backlight
+		AddPointLight({ -2.5f,	5.f,	-5.f }, 70.f, ColorRGB{ 1.f, 0.8f, 0.45f }); //Front Light Left
+		AddPointLight({ 2.5f,	2.5f,	-5.f }, 50.f, ColorRGB{ 0.34f, 0.47f, 0.68f });
+	}
+	void Scene_W4_TestScene::Update(Timer* pTimer)
+	{
+		Scene::Update(pTimer);
+
+		pMesh->RotateY(PI_DIV_2 * pTimer->GetTotal());
+		pMesh->UpdateTransforms();
+	}
 #pragma endregion
 }
