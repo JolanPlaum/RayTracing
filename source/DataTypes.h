ce794796aee5256c8380b1e7d9ef2dcf816db0b3
Jolan Plaum <jolan.plaum@student.howest.be>
2022-11-07 17:12:35 +0100

    Implemented TriangleMesh functionality

--- source/DataTypes.h
+++ source/DataTypes.h
@@ -123,20 +123,62 @@ namespace dae
 
 		void CalculateNormals()
 		{
-			assert(false && "No Implemented Yet!");
+			//Clear all existing normals
+			normals.clear();
+
+			//Reserve capacity equal to amount of triangles
+			normals.reserve(int(indices.size() / 3));
+
+			//Loop over all indices in sets of 3 (each triangle has 3 points)
+			for (size_t index{}; index < indices.size() - 2; index += 3)
+			{
+				size_t i0 = indices[index];
+				size_t i1 = indices[index + 1];
+				size_t i2 = indices[index + 2];
+
+				Vector3 edgeV0V1 = positions[i1] - positions[i0];
+				Vector3 edgeV0V2 = positions[i2] - positions[i0];
+
+				normals.emplace_back(Vector3::Cross(edgeV0V1, edgeV0V2).Normalized());
+			}
 		}
 
 		void UpdateTransforms()
 		{
-			assert(false && "No Implemented Yet!");
+			//Clear all existing transformed
+			transformedPositions.clear();
+			transformedNormals.clear();
+
+			//Reserve capacity equal to amount of triangles
+			transformedPositions.reserve(positions.size());
+			transformedNormals.reserve(normals.size());
+
+			//Resize equal to amount
+			//transformedPositions.resize(positions.size());
+			//transformedNormals.resize(normals.size());
+
 			//Calculate Final Transform 
-			//const auto finalTransform = ...
+			const Matrix finalTransform = scaleTransform * rotationTransform * translationTransform;
 
 			//Transform Positions (positions > transformedPositions)
-			//...
+			//for (size_t i{}; i < positions.size(); ++i)
+			//{
+			//	transformedPositions[i] = std::move(finalTransform.TransformPoint(positions[i]));
+			//}
+			for (const Vector3& pos : positions)
+			{
+				transformedPositions.emplace_back(finalTransform.TransformPoint(pos));
+			}
 
 			//Transform Normals (normals > transformedNormals)
-			//...
+			//for (size_t i{}; i < normals.size(); ++i)
+			//{
+			//	transformedNormals[i] = std::move(finalTransform.TransformVector(normals[i]));
+			//}
+			for (const Vector3& n : normals)
+			{
+				transformedNormals.emplace_back(finalTransform.TransformVector(n));
+			}
 		}
 	};
 #pragma endregion
